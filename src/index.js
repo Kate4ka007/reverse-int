92678eba813a7acae6e6c1f04d6e08bebda59944
Kate4ka007 <shlemena@mail.ru>
2022-01-13 00:57:39 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+   
+  let g = Math.abs(n)
+  let m =  g.toString();
+  let i = m.length - 1;
+
+  let str = '';
+  while (i >= 0) {
+      str = `${str}${m[i]}`;
+      i = i - 1;
+  }
+  return str;
+};
